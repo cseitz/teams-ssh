eb8c6ea5d17034745993e34d6654dd292ae35867
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-18 15:20:41 -0500

    wtf

--- example.config.ts
+++ example.config.ts
@@ -5,15 +5,15 @@ import { getMembersDelta } from './src/teams';
 
 async function updateSSH() {
     const { added, removed, members, commit } = await getMembersDelta('ssh');
-    await removed.map(o => ssh('disable', o.login));
-    await added.map(o => ssh('enable', o.login));
+    await Promise.all(removed.map(o => ssh('disable', o.login)));
+    await Promise.all(added.map(o => ssh('enable', o.login)));
     await commit();
 }
 
 async function updateSudo() {
     const { added, removed, members, commit } = await getMembersDelta('sudo');
-    await removed.map(o => sudo('disable', o.login));
-    await added.map(o => sudo('enable', o.login));
+    await Promise.all(removed.map(o => sudo('disable', o.login)));
+    await Promise.all(added.map(o => sudo('enable', o.login)));
     await commit();
 }
 
